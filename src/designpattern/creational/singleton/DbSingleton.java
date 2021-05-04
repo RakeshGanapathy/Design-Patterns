c7c423327602ea7d7e7ce8c0c8bc2b9400dc9a94
admin <admin@DESKTOP-VFVIQ95>
2021-05-04 09:32:42 +0530

    Singleton improvised

--- src/designpattern/creational/singleton/DbSingleton.java
+++ src/designpattern/creational/singleton/DbSingleton.java
@@ -1,30 +1,35 @@
 package designpattern.creational.singleton;
 
 public class DbSingleton {
-	
+
+	private static volatile DbSingleton instance;
+
 	private DbSingleton() {
-		if(instance!=null) {
+		if (instance != null) {
 			throw new RuntimeException("use get instance method to create");
 		}
 	}
-	
-	private static volatile DbSingleton instance;
-	
-	public synchronized static DbSingleton getInstance() { 
-		// lazy instantiation 
-		if(instance==null) {
-			instance = new DbSingleton();
+
+	public static DbSingleton getInstance() {
+		// lazy instantiation
+		if (instance == null) {
+			// thread safe check
+			synchronized (DbSingleton.class) {
+				if (instance == null) {
+					instance = new DbSingleton();
+				}
+			}
 		}
 		return instance;
 	}
 }
 
 class Implementation {
-	public static void main (String [] args) {
+	public static void main(String[] args) {
 		System.out.println("main method");
-		DbSingleton ref= DbSingleton.getInstance();
+		DbSingleton ref = DbSingleton.getInstance();
 		System.out.println(ref);
-		DbSingleton ref1= DbSingleton.getInstance();
+		DbSingleton ref1 = DbSingleton.getInstance();
 		System.out.println(ref1);
 	}
 }
